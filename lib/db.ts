ac063afb92560a5a86dd7b6ed5f8c3de61f89bc2
Artem <artemdenisenko@gmail.com>
2026-05-19 21:37:54 +0200

    Make lib/db.ts safe to import during build

--- lib/db.ts
+++ lib/db.ts
@@ -1,30 +1,28 @@
-﻿import { setServers } from 'dns'
+﻿import { setServers } from "dns"
 setServers(["8.8.8.8", "8.8.4.4"])
 
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb"
 
-const uri = process.env.MONGODB_URI as string
+const uri = process.env.MONGODB_URI || ""
 const options = {}
 
-let client: MongoClient
 let clientPromise: Promise<MongoClient>
 
-if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local')
-}
-
-if (process.env.NODE_ENV === 'development') {
-  let globalWithMongo = global as typeof globalThis & {
-    _mongoClientPromise?: Promise<MongoClient>
+try {
+  const client = new MongoClient(uri, options)
+  if (process.env.NODE_ENV === "development") {
+    const globalWithMongo = global as typeof globalThis & {
+      _mongoClientPromise?: Promise<MongoClient>
+    }
+    if (!globalWithMongo._mongoClientPromise) {
+      globalWithMongo._mongoClientPromise = client.connect()
+    }
+    clientPromise = globalWithMongo._mongoClientPromise
+  } else {
+    clientPromise = client.connect()
   }
-  if (!globalWithMongo._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    globalWithMongo._mongoClientPromise = client.connect()
-  }
-  clientPromise = globalWithMongo._mongoClientPromise
-} else {
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+} catch (e) {
+  clientPromise = Promise.reject(e)
 }
 
 export default clientPromise
